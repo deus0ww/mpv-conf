fb1472fbec8467c5368707608740897e2d2801be
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-01-28 03:00:23 +0700

    Various changes

--- scripts/Equalizer_Profiles.lua
+++ scripts/Equalizer_Profiles.lua
@@ -1,44 +1,65 @@
--- deus0ww - 2019-01-22
+-- deus0ww - 2019-01-26
 
 local mp      = require 'mp'
 local msg     = require 'mp.msg'
+local opt     = require 'mp.options'
 local utils   = require 'mp.utils'
+
+local user_opts = {
+	reset_on_load = true,
+}
+opt.read_options(user_opts, mp.get_script_name())
 
 local eqs    = { 'brightness', 'contrast', 'gamma', 'hue', 'saturation' }
 local lables = { brightness = 'Brightness', contrast = 'Contrast', gamma = 'Gamma', hue = 'Hue', saturation = 'Saturation' }
 
 local profiles = { current_index = 1, last_index = 1, limit = 1 }
-for i = 0, profiles.limit do 
-	profiles[i] = {}
+
+local function reset_profile(profile)
 	for _, eq in pairs(eqs) do
-		profiles[i][eq] = 0
-	end	
+		profile[eq] = 0
+	end
 end
 
-local function show_status(eq)
-	local profile = profiles[profiles.current_index]
+local function reset_all()
+	for i = 0, profiles.limit do 
+		if not profiles[i] then profiles[i] = {} end
+		reset_profile(profiles[i])
+	end
+	profiles.current_index = 1
+	profiles.last_index    = 1
+end
+reset_all()
+
+local function show_status(profile, eq)
 	local id_string = ('%s EQ %d'):format(((profiles.current_index ~= 0) and '☑︎' or '☐'), profiles.current_index == 0 and profiles.last_index or profiles.current_index)
 	local eq_string = eq and ('%s %+.2d'):format(lables[eq], profile[eq])
 						 or  ('Brightness %+.2d, Contrast %+.2d, Gamma %+.2d, Hue %+.2d, Saturation %+.2d'):format(profile.brightness, profile.contrast, profile.gamma, profile.hue, profile.saturation)
 	mp.osd_message(('%s: %s'):format(id_string, eq_string))
 end
 
-local function set_eq()
-	for _, eq in ipairs(eqs) do
-		mp.set_property_number(eq, profiles[profiles.current_index][eq])
+local function apply_profile(profile, eq)
+	if eq then  -- Set one value
+		mp.set_property_number(eq, profile[eq] and profile[eq] or 0)
+	else        -- Set all values
+		for _, eqx in pairs(eqs) do
+			mp.set_property_number(eqx, profile[eqx] and profile[eqx] or 0)
+		end
 	end
 end
 
 mp.register_script_message('Eq-cycle-', function()
 	profiles.current_index = ((profiles.current_index - 2) % profiles.limit) + 1
-	set_eq()
-	show_status()
+	local profile = profiles[profiles.current_index]
+	apply_profile(profile)
+	show_status(profile)
 end)
 
 mp.register_script_message('Eq-cycle+', function()
 	profiles.current_index = (profiles.current_index % profiles.limit) + 1
-	set_eq()
-	show_status()
+	local profile = profiles[profiles.current_index]
+	apply_profile(profile)
+	show_status(profile)
 end)
 
 mp.register_script_message('Eq-toggle', function()
@@ -48,23 +69,35 @@ mp.register_script_message('Eq-toggle', function()
 	else
 		profiles.current_index = profiles.last_index
 	end
-	set_eq()
-	show_status()
+	local profile = profiles[profiles.current_index]
+	apply_profile(profile)
+	show_status(profile)
 end)
 
 mp.register_script_message('Eq-reset', function()
 	if profiles.current_index == 0 then profiles.current_index = profiles.last_index end
-	for _, eq in pairs(eqs) do
-		profiles[profiles.current_index][eq] = 0
+	local profile = profiles[profiles.current_index]
+	reset_profile(profile)
+	apply_profile(profile)
+	show_status(profile)
+end)
+
+mp.register_event("file-loaded", function()
+	local profile = profiles[profiles.current_index]
+	if user_opts.reset_on_load then 
+		reset_all()
+		apply_profile(profile)
+	else
+		apply_profile(profile)
+		show_status(profile)
 	end
-	set_eq()
-	show_status()
 end)
 
 local function on_eq_set(eq, value)
 	profiles[profiles.current_index][eq] = math.min(math.max(value, -100), 100)
-	set_eq()
-	show_status(eq)
+	local profile = profiles[profiles.current_index]
+	apply_profile(profile)
+	show_status(profile, eq)
 end
 
 local function on_eq_change(eq, amount)
@@ -73,7 +106,7 @@ local function on_eq_change(eq, amount)
 	on_eq_set(eq, profiles[profiles.current_index][eq] + amount)
 end
 
-for _, eq in ipairs(eqs) do
+for _, eq in pairs(eqs) do
 	mp.register_script_message('Eq-' .. eq,           function(amount) on_eq_change(eq, amount) end)
 	mp.register_script_message('Eq-' .. eq .. '-set', function(value)  on_eq_set   (eq, value)  end)
 end
