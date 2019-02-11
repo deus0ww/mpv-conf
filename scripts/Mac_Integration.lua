dc41618eaba845e15f3f8e5206691aa302a9e6ac
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-02-11 22:18:15 +0700

    Update Mac_Integration.lua
    Pause on Minimize doesn't work on mac with vo=libmpv

--- scripts/Mac_Integration.lua
+++ scripts/Mac_Integration.lua
@@ -1,4 +1,4 @@
--- deus0ww - 2019-02-07
+-- deus0ww - 2019-02-11
 
 local mp      = require 'mp'
 local msg     = require 'mp.msg'
@@ -7,7 +7,7 @@ local msg     = require 'mp.msg'
 
 -- Show File in Finder
 mp.register_script_message('ShowInFinder', function()
-	local path = mp.get_property('path', ''):gsub('edl://', ''):gsub(';/', '" /"')
+	local path = mp.get_property_native('path', ''):gsub('edl://', ''):gsub(';/', '" /"')
 	msg.debug('Show in Finder:', path)
 	if path and path ~= '' then os.execute( ('open -R "%s"'):format(path) ) end
 end)
@@ -17,30 +17,30 @@ end)
 -- OnTop only while playing
 local PROP_ONTOP = 'ontop'
 local PROP_PAUSE = 'pause'
-local ontop      = mp.get_property(PROP_ONTOP)
-local paused     = mp.get_property_bool(PROP_PAUSE)
+local last_ontop = mp.get_property_native(PROP_ONTOP, false)
+local last_pause = mp.get_property_native(PROP_PAUSE, false)
 
-mp.observe_property(PROP_PAUSE, 'bool', function(paused)
-    if paused then
-		ontop = mp.get_property(PROP_ONTOP)
-		mp.set_property(PROP_ONTOP, 'no')
+mp.observe_property(PROP_PAUSE, 'native', function(_, pause)
+	msg.debug('Pause:', pause)
+    if pause then
+    	msg.debug('Paused - Disabling OnTop')
+		last_ontop = mp.get_property_native(PROP_ONTOP)
+		mp.set_property_native(PROP_ONTOP, false)
 	else
-		mp.set_property(PROP_ONTOP, ontop)
+		msg.debug('Unpaused - Restoring OnTop')
+		mp.set_property_native(PROP_ONTOP, last_ontop)
     end
 end)
 
-mp.observe_property(PROP_ONTOP, 'bool', function(current_ontop)
-	if paused and current_ontop then ontop = current_ontop end
-end)
-
-
-
 -- Pause on Minimize
---	mp.observe_property('window-minimized', 'bool', function(minimized)
+--	mp.observe_property('window-minimized', 'native', function(_, minimized)
+--		msg.debug('Minimized:', minimized)
 --		if minimized then
---			paused = mp.get_property_bool(PROP_PAUSE)
---			mp.set_property_bool(PROP_PAUSE, 'yes')
+--			msg.debug('Minimized - Pausing')
+--			last_pause = mp.get_property_native(PROP_PAUSE, false)
+--			mp.set_property_native(PROP_PAUSE, true)
 --		else
---			mp.set_property_bool(PROP_PAUSE, paused)
+--			msg.debug('Unminimized - Restoring Pause')
+--			mp.set_property_native(PROP_PAUSE, last_pause)
 --		end
 --	end)
