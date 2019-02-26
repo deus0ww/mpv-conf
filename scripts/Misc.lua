01c5dd56aeaa91d845ac74e63c0245b9ca58e675
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-02-27 05:29:47 +0700

    Show-Cache: simplify text when cache is disabled; code clean up

--- scripts/Misc.lua
+++ scripts/Misc.lua
@@ -43,15 +43,26 @@ local function show_cache_status()
 	local demux_state      = mp.get_property_native('demuxer-cache-state', {})
 	local demux_fwd        = math.floor( ((demux_state and demux_state['fw-bytes'])    and demux_state['fw-bytes']    or 0) / 1048576 + 0.5 )
 	local demux_total      = math.floor( ((demux_state and demux_state['total-bytes']) and demux_state['total-bytes'] or 0) / 1048576 + 0.5 )
+	local demux_ranges     = #demux_state['seekable-ranges']
 	local demux_duration   = math.floor(  mp.get_property_native('demuxer-cache-duration', 0) + 0.5 )
 	local demux_network    = mp.get_property_native('demuxer-via-network', false)
 	local paused_for_cache = mp.get_property_native('paused-for-cache', false)
 	local buffering_state  = math.floor( mp.get_property_native('cache-buffering-state', 0) + 0.5 )
 	
-	local cache_string     = ('%s Cache: %.2d/%.2d MiB   '):format(mark[cache], cache_used, cache_size)
-	local demux_string     = ('Demuxer: %.2d/%.2d MiB (%dm%.2ds)   '):format(demux_fwd, demux_total, math.floor(demux_duration / 60), math.floor(demux_duration % 60))
-	local speed_string     = demux_network and ((cache_speed < 1048576) and ('Speed: %s KB/s   '):format(math.floor(cache_speed / 1024)) or ('Speed: %s MB/s   '):format(math.floor(cache_speed / 1048576))) or ''
-	local pause_string     = paused_for_cache and ('Paused for buffering... %d%%   '):format(buffering_state) or ''
+	local cache_string     = cache_size > 0
+	                         and ('%s Cache: %.2d/%.2d MiB   '):format(mark[cache], cache_used, cache_size)
+	                         or  ('%s Cache   '):format(mark[cache])
+	local demux_string     = demux_ranges > 0
+	                         and ('Demuxer: %.2d/%.2d MiB (%dm%.2ds)   '):format(demux_fwd, demux_total, math.floor(demux_duration / 60), math.floor(demux_duration % 60))
+	                         or  ''
+	local speed_string     = demux_network
+	                         and ((cache_speed < 1048576) 
+	                             and ('Speed: %s KB/s   '):format(math.floor(cache_speed / 1024))
+	                             or  ('Speed: %s MB/s   '):format(math.floor(cache_speed / 1048576)))
+	                         or  ''
+	local pause_string     = paused_for_cache
+	                         and ('Paused for buffering... %d%%   '):format(buffering_state)
+	                         or  ''
 	
 	mp.osd_message( cache_string .. demux_string  .. speed_string .. pause_string)
 end
