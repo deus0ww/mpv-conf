1e5b6176cf5a6059a540b73dc51dda5680ffd4f6
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-03-12 04:52:56 +0700

    show when cache is disabled

--- scripts/Misc.lua
+++ scripts/Misc.lua
@@ -47,8 +47,8 @@ local function show_cache_status()
 	local buffering_state  = math.floor( mp.get_property_native('cache-buffering-state', 0) + 0.5 )
 	
 	local demux_string     = demux_ranges > 0
-	                         and ('Cache: %.2d/%.2d MiB (%dm%.2ds)   '):format(demux_fwd, demux_total, math.floor(demux_duration / 60), math.floor(demux_duration % 60))
-	                         or  ''
+	                         and ('☑︎ Cache: %.2d/%.2d MiB (%dm%.2ds)   '):format(demux_fwd, demux_total, math.floor(demux_duration / 60), math.floor(demux_duration % 60))
+	                         or   '☐ Cache'
 	local speed_string     = demux_network
 	                         and ((cache_speed < 1048576) 
 	                             and ('Speed: %s KB/s   '):format(math.floor(cache_speed / 1024))
