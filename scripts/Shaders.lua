c118db4fd81043195c30570ca13e3d37313ee8e9
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-04-21 06:16:01 +0700

    Shaders.lua - Cleanup, added SSSR

--- scripts/Shaders.lua
+++ scripts/Shaders.lua
@@ -19,33 +19,34 @@ local function reset()
 		['height'] = 0,
 		['osd-width'] = 0,
 		['osd-height'] = 0,
-		['video-params/chroma-location'] = 'unknown',
+		['video-params/chroma-location'] = 0,
 	}
-	last_shaders = ''
+	last_shaders = nil
 end
 reset()
 
+local ravu_limit = 3
+local high_fps_threshold = 33
+local function get_scale(p) return math.min( p['osd-width'] / p['width'], p['osd-height'] / p['height'] ) end
+local function use_ravu(p) return get_scale(p) >= ravu_limit end
+local function is_high_fps(p) return p['container-fps'] > high_fps_threshold end
+local function is_chroma_left(p)   return p['video-params/chroma-location'] == 'mpeg2/4/h264' end
+local function is_chroma_center(p) return p['video-params/chroma-location'] == 'mpeg1/jpeg'   end
+
 local function create_shaders()
 	local p, s = watched_properties, {}
-	
+	local scale, ravu = get_scale(p), use_ravu(p)
+
 	-- LUMA
-	if p['container-fps'] <= 30 then
-		s[#s+1] = 'FSRCNNX_x2_16-0-4-1.glsl'
-	else
-		s[#s+1] = 'FSRCNNX_x2_8-0-4-1.glsl'
-	end
-	s[#s+1] = 'ravu-zoom-r4.hook'
+	s[#s+1] = is_high_fps(p) and 'FSRCNNX_x2_8-0-4-1.glsl' or 'FSRCNNX_x2_16-0-4-1.glsl'
+	if ravu then s[#s+1] = 'ravu-zoom-r4.hook' end
 	s[#s+1] = 'EnhanceDetail.glsl'
-	
 	-- Chroma
-	if math.min( p['osd-width'] / p['width'], p['osd-height'] / p['height'] ) > 2 then
-		if     p['video-params/chroma-location'] == 'mpeg2/4/h264' then
-			s[#s+1] = 'ravu-r4-chroma-left.hook'
-		elseif p['video-params/chroma-location'] == 'mpeg1/jpeg'   then
-			s[#s+1] = 'ravu-r4-chroma-center.hook'
-		end
-	end
+	if ravu and is_chroma_left(p)   then s[#s+1] = 'ravu-r4-chroma-left.hook'   end
+	if ravu and is_chroma_center(p) then s[#s+1] = 'ravu-r4-chroma-center.hook' end
 	s[#s+1] = 'KrigBilateral.glsl'
+	-- RGB
+	if not ravu then s[#s+1] = 'SSimSuperRes.glsl' end
 
 	return s
 end
@@ -77,17 +78,14 @@ local timer = mp.add_timeout(0.5, function() set_shaders(create_shaders()) end)
 timer:kill()
 for prop, _ in pairs(watched_properties) do
 	mp.observe_property(prop, 'native', function(_, v_new)
-		local p = watched_properties
-		if v_new == nil or v_new == p[prop] then return end
-		msg.debug('Property', prop, 'changed:', p[prop], '->', v_new)
-		p[prop] = v_new
+		msg.debug('Property', prop, 'changed:', watched_properties[prop], '->', v_new)
 		
-		if  p['container-fps'] == 0 or
-			p['width']         == 0 or
-			p['height']        == 0 or
-			p['osd-width']     == 0 or
-			p['osd-height']    == 0
-		then return end
+		if v_new == nil or v_new == watched_properties[prop] then return end
+		watched_properties[prop] = v_new
+		
+		for _, value in pairs(watched_properties) do
+			if value == 0 then return end
+		end
 
 		msg.debug('Resetting Timer')
 		timer:kill()
