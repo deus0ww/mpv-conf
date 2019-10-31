ccf869afdf75979b33ffdf57843a9445a8d59102
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-10-31 13:21:23 +0700

    Shaders - Auto switch set based on path

--- scripts/Shaders.lua
+++ scripts/Shaders.lua
@@ -7,38 +7,28 @@ local utils   = require 'mp.utils'
 
 
 local user_opts = {
-	enabled = false,
-	set = 1,
-	path = '/Users/Shared/Library/mpv/shaders/', -- Should be the same as '~~/shaders/' expanded
+	enabled     = false,
+	auto_switch = true,
+	general_set = 1,
+	anime_set   = 2,
+	path        = mp.command_native({'expand-path', '~~/shaders/'}),
 }
-
-
--------------
---- Utils ---
--------------
-local function file_exists(path)
-	local file = io.open(path, 'rb')
-	if not file then return false end
-	local _, _, code = file:read(1)
-	file:close()
-	return code == nil
-end
 
 
 ------------------
 --- Properties ---
 ------------------
+local current_set = user_opts.general_set
 local props, last_shaders
 local function reset()
 	props = {
-		['width'] = '',
-		['height'] = '',
-		['osd-width'] = 0,
-		['osd-height'] = 0,
-		['container-fps'] = '',
+		['width']               = '',
+		['height']              = '',
+		['osd-width']           = 0,
+		['osd-height']          = 0,
+		['container-fps']       = '',
 		['video-params/rotate'] = '',
 	}
-	last_shaders = nil
 end
 reset()
 
@@ -125,36 +115,50 @@ end
 --------------------
 --- MPV Commands ---
 --------------------
-local function clear_shaders()
+local function show_osd(no_osd, label)
+	if no_osd then return end
+	mp.osd_message(('%s Shaders Set %d: %s'):format(user_opts.enabled and '☑︎' or '☐', current_set, label or 'n/a'), 6)
+end
+
+local function mpv_clear_shaders()
 	msg.debug('Clearing Shaders.')
 	mp.commandv('change-list', 'glsl-shaders', 'clr', '')
 end
 
-local function apply_shaders(shaders)
+local function mpv_set_shaders(shaders)
+	mpv_clear_shaders()
+	msg.debug('Setting Shaders:', utils.to_string(shaders))
+	for _, shader in ipairs(shaders) do
+		if shader and shader ~= '' then mp.commandv('change-list', 'glsl-shaders', 'append', '~~/shaders/' .. shader) end
+	end
+end
+
+local function clear_shaders(no_osd)
+	local shaders = sets[current_set]()
+	show_osd(no_osd, shaders.label)
+	if last_shaders == nil then
+		msg.debug('Clearing Shaders: skipped - no shader found.')
+		return
+	end
+	last_shaders = nil
+	mpv_clear_shaders()
+end
+
+local function set_shaders(no_osd)
+	local shaders = sets[current_set]()
+	show_osd(no_osd, shaders.label)
 	if not user_opts.enabled then
 		msg.debug('Setting Shaders: skipped - disabled.')
 		return
 	end
-
 	local s, _ = utils.to_string(shaders)
 	if last_shaders == s then 
 		msg.debug('Setting Shaders: skipped - shaders unchanged.')
 		return
 	end
 	last_shaders = s
-
-	clear_shaders()
-	msg.debug('Setting Shaders:', s)
-	for _, shader in ipairs(shaders) do
-		if shader and shader ~= '' then mp.commandv('change-list', 'glsl-shaders', 'append', '~~/shaders/' .. shader) end
-	end
+	mpv_set_shaders(shaders.shaders)
 end
-
-local function show_osd(no_osd, label)
-	if no_osd then return end
-	mp.osd_message(('%s Shaders Set %d: %s'):format(user_opts.enabled and '☑︎' or '☐', user_opts.set, label or 'n/a'), 6)
-end
-
 
 --------------------------
 --- Observers & Events ---
@@ -162,9 +166,13 @@ end
 mp.register_event('file-loaded', function() 
 	opt.read_options(user_opts, mp.get_script_name())
 	reset()
+	local path = mp.get_property_native('path', ''):lower()
+	if user_opts.auto_switch then
+		current_set = (path:find('anime') ~= nil) and user_opts.anime_set or user_opts.general_set
+	end
 end)
 
-local timer = mp.add_timeout(1, function() apply_shaders(sets[user_opts.set]().shaders) end)
+local timer = mp.add_timeout(1, function() set_shaders(true) end)
 timer:kill()
 for prop, _ in pairs(props) do
 	mp.observe_property(prop, 'native', function(_, v_new)
@@ -186,52 +194,38 @@ end
 --- Bindings ---
 ----------------
 local function cycle_set_up(no_osd)
-	msg.debug('Shader - Up:', user_opts.set)
+	msg.debug('Shader - Up:', current_set)
 	if not is_initialized() then return end
-	user_opts.set = (user_opts.set % #sets) + 1
-	local shaders = sets[user_opts.set]()
-	if user_opts.enabled then apply_shaders(shaders.shaders) end
-	show_osd(no_osd, shaders.label)
+	current_set = (current_set % #sets) + 1
+	set_shaders(no_osd)
 end
 
 local function cycle_set_dn(no_osd)
-	msg.debug('Shader - Down:', user_opts.set)
+	msg.debug('Shader - Down:', current_set)
 	if not is_initialized() then return end
-	user_opts.set = ((user_opts.set - 2) % #sets) + 1
-	local shaders = sets[user_opts.set]()
-	if user_opts.enabled then apply_shaders(shaders.shaders) end
-	show_osd(no_osd, shaders.label)
+	current_set = ((current_set - 2) % #sets) + 1
+	set_shaders(no_osd)
 end
 
 local function toggle_set(no_osd)
-	msg.debug('Shader - Toggling:', user_opts.set)
+	msg.debug('Shader - Toggling:', current_set)
 	if not is_initialized() then return end
-	if user_opts.set == 0 then user_opts.set = 1 end
-	last_shaders = nil
-	local shaders = sets[user_opts.set]()
-	if user_opts.enabled then clear_shaders() else apply_shaders(shaders.shaders) end
 	user_opts.enabled = not user_opts.enabled
-	show_osd(no_osd, shaders.label)
+	if user_opts.enabled then set_shaders(no_osd) else clear_shaders(no_osd) end
 end
 
 local function enable_set(no_osd)
-	msg.debug('Shader - Enabling:', user_opts.set)
+	msg.debug('Shader - Enabling:', current_set)
 	if not is_initialized() then return end
 	user_opts.enabled = true
-	last_shaders = nil
-	local shaders = sets[user_opts.set]()
-	apply_shaders(shaders.shaders)
-	show_osd(no_osd, shaders.label)
+	set_shaders(no_osd)
 end
 
 local function disable_set(no_osd)
-	msg.debug('Shader - Disabling:', user_opts.set)
+	msg.debug('Shader - Disabling:', current_set)
 	if not is_initialized() then return end
 	user_opts.enabled = false
-	last_shaders = nil
-	local shaders = sets[user_opts.set]()
-	clear_shaders()
-	show_osd(no_osd, shaders.label)
+	clear_shaders(no_osd)
 end
 
 mp.register_script_message('Shaders-cycle+',  function(no_osd) cycle_set_up(no_osd == 'yes') end)
