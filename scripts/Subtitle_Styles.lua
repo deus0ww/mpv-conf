169cd168079d5b7a4aad26c1fc82f6eaffb25eab
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-11-06 18:04:40 +0700

    Subtitle_Style - Added profile for bitmap subs

--- scripts/Subtitle_Styles.lua
+++ scripts/Subtitle_Styles.lua
@@ -1,4 +1,4 @@
--- deus0ww - 2019-05-23
+-- deus0ww - 2019-11-06
 
 local mp      = require 'mp'
 local msg     = require 'mp.msg'
@@ -17,14 +17,25 @@ local language_profiles = {
 	und = 'ww-subtitle-english',
 }
 
+local codec_type = {
+	dvb_subtitle = 'bitmap',
+	dvd_subtitle = 'bitmap',
+	hdmv_pgs_subtitle = 'bitmap',
+}
+setmetatable(codec_type, {__index = function() return 'text' end})
+
+local codec_profile = {
+	bitmap = 'ww-subtitle-bitmap',
+	text   = 'ww-subtitle-text',
+}
+
 local function get_sub_language(track_list, sid)
 	for _, track in ipairs(track_list) do
-		msg.error('track.default', track.default, 'track.lang', track.lang)
 		if track.type == 'sub' and track.lang and track.lang ~= '' and
 		   (sid == track.id or (sid == 'auto' and track.default)) then
-			for lang, _ in pairs(language_codes) do
-				if language_codes[lang]:find(track.lang:lower()) ~= nil then
-					return lang
+			for code, desc in pairs(language_codes) do
+				if desc:find(track.lang:lower()) ~= nil then
+					return code
 				end
 			end
 		end
@@ -32,12 +43,29 @@ local function get_sub_language(track_list, sid)
 	return 'und'
 end
 
+local function get_sub_type(track_list, sid)
+	for _, track in ipairs(track_list) do
+		if track.type == 'sub' and track.codec and track.codec ~= '' and
+		   (sid == track.id or (sid == 'auto' and track.default)) then
+		   return codec_type[track.codec:lower()]
+		end
+	end
+end
+
 mp.observe_property('sid', 'native', function(_, sid)
 	if not sid or sid == 'no' then return end
 	local track_list = mp.get_property_native('track-list', {})
 	if #track_list <= 0 then return end
+	
+	-- Set Language Style
 	local track_lang = get_sub_language(track_list, sid)
-	local profile = language_profiles[track_lang]
-	msg.debug(('SID: %s, Language: %s, Profile: %s'):format(sid, track_lang, profile))
-	mp.commandv('apply-profile', profile)
+	local profile_lang = language_profiles[track_lang]
+	mp.commandv('apply-profile', profile_lang)
+	
+	-- Set Codec Style
+	local track_type = get_sub_type(track_list, sid)
+	local profile_type = codec_profile[track_type]
+	mp.commandv('apply-profile', profile_type)
+	
+	msg.debug(('SID: %s, Language: %s, Type: %s, Profiles: %s, %s'):format(sid, track_lang, track_type, profile_lang, profile_type))
 end)
