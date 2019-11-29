b7ebb6fb68cf8c7c1da0c3dbe705067f309ee18a
deus0ww <6702585+deus0ww@users.noreply.github.com>
2019-11-29 20:59:33 +0700

    Shaders - Set related options from within the script

--- scripts/Shaders.lua
+++ scripts/Shaders.lua
@@ -1,4 +1,4 @@
--- deus0ww - 2019-11-24
+-- deus0ww - 2019-11-29
 
 local mp      = require 'mp'
 local msg     = require 'mp.msg'
@@ -6,7 +6,7 @@ local opt     = require 'mp.options'
 local utils   = require 'mp.utils'
 
 
-local o = {
+local opts = {
 	enabled          = false,    -- Master switch to enable/disable shaders
 
 	default_index    = 1,        -- Default shader set
@@ -24,13 +24,13 @@ local o = {
 	preset_3_path    = 'cartoon',
 	preset_3_index   = 3,
 }
-opt.read_options(o, mp.get_script_name())
+opt.read_options(opts, mp.get_script_name())
 
 
 ------------------
 --- Properties ---
 ------------------
-local current_set, enabled = o.default_index, o.enabled
+local current_index, enabled = opts.default_index, opts.enabled
 local props, last_shaders
 local function reset()
 	props = {
@@ -61,6 +61,15 @@ local function get_scale()
 	if (props['video-params/rotate'] % 180) ~= 0 then width, height = height, width end
 	return math.min( mp.get_property_native('osd-width', 0) / width, mp.get_property_native('osd-height', 0) / height )
 end
+local function default_options()
+	return {
+		['scale']  = 'ewa_lanczossharp',
+		['cscale'] = 'ewa_robidouxsharp',
+		['dscale'] = 'ewa_robidouxsharp',
+		['linear-downscaling'] = 'yes',
+		['sigmoid-upscaling']  = 'yes',
+	}
+end
 
 
 -------------------
@@ -69,7 +78,7 @@ end
 local sets = {}
 
 sets[#sets+1] = function()
-	local s = {}
+	local s, o = {}, default_options()
 	-- Luma
 	s[#s+1] = is_high_fps() and 'FSRCNNX_x2_8-0-4-1.glsl' or 'FSRCNNX_x2_16-0-4-1.glsl'
 	s[#s+1] = 'ravu-lite-r4.hook'
@@ -79,30 +88,42 @@ sets[#sets+1] = function()
 	s[#s+1] = 'SSimSuperRes.glsl'
 	s[#s+1] = 'SSimDownscaler.glsl'
 	s[#s+1] = 'adaptive-sharpen.glsl'
-	return { shaders = s, label = 'FSRCNNX + RAVU-Lite + Krig + SSimSR/DS + AdaptiveSharpen' }
+	-- Options
+	o['dscale'] = 'ewa_robidoux'    -- For SSimDownscaler.glsl
+	o['linear-downscaling'] = 'no'  -- For SSimDownscaler.glsl
+	o['sigmoid-upscaling']  = 'no'  -- For adaptive-sharpen.glsl
+	
+	return { shaders = s, options = o, label = 'FSRCNNX + RAVU-Lite + Krig + SSimSR/DS + AdaptiveSharpen' }
 end
 
 sets[#sets+1] = function()
-	local s, label = {}
-	if get_scale() <= 2 then
+	local s, o, scale, label = {}, default_options(), get_scale()
+	if scale < 1 then
+		s[#s+1] = 'SSimDownscaler.glsl'
+		o['dscale'] = 'ewa_robidoux'    -- For SSimDownscaler.glsl
+		o['linear-downscaling'] = 'no'  -- For SSimDownscaler.glsl
+		label   = 'Krig + SSimDS + AdaptiveSharpen'
+	elseif scale <= 2 then
 		s[#s+1] = 'Anime4K_Adaptive_v1.0RC2.glsl'
 		s[#s+1] = 'FSRCNNX_x2_8-0-4-1.glsl'
-		label   = 'Anime4K + FSRCNNX'
+		label   = 'Anime4K + FSRCNNX + Krig + AdaptiveSharpen'
 	else
 		s[#s+1] = 'FSRCNNX_x2_8-0-4-1.glsl'
 		s[#s+1] = 'SSimSuperRes.glsl'
 		s[#s+1] = 'Anime4K_Adaptive_v1.0RC2.glsl'
 		s[#s+1] = 'ravu-lite-r4.hook'
-		label   = 'FSRCNNX + Anime4K + RAVU-Lite'
+		label   = 'FSRCNNX + Anime4K + RAVU-Lite + Krig + SSimSR + AdaptiveSharpen'
 	end
 	s[#s+1] = 'KrigBilateral.glsl'
-	s[#s+1] = 'SSimDownscaler.glsl'
 	s[#s+1] = 'adaptive-sharpen.glsl'
-	return { shaders = s, label = label .. ' + Krig + SSimSR/DS + AdaptiveSharpen' }
+	
+	o['sigmoid-upscaling']  = 'no'  -- For adaptive-sharpen.glsl
+	
+	return { shaders = s, options = o, label = label }
 end
 
 sets[#sets+1] = function()
-	local s = {}
+	local s, o = {}, default_options()
 	-- Luma
 	s[#s+1] = is_high_fps() and 'FSRCNNX_x2_8-0-4-1.glsl' or 'FSRCNNX_x2_16-0-4-1.glsl'
 	s[#s+1] = 'ravu-lite-r4.hook'
@@ -111,7 +132,11 @@ sets[#sets+1] = function()
 	-- RGB
 	s[#s+1] = 'SSimSuperRes.glsl'
 	s[#s+1] = 'SSimDownscaler.glsl'
-	return { shaders = s, label = 'FSRCNNX + RAVU-Lite + Krig + SSimSR/DS' }
+	-- Options
+	o['dscale'] = 'ewa_robidoux'    -- For SSimDownscaler.glsl
+	o['linear-downscaling'] = 'no'  -- For SSimDownscaler.glsl
+	
+	return { shaders = s, options = o, label = 'FSRCNNX + RAVU-Lite + Krig + SSimSR/DS' }
 end
 
 
@@ -120,7 +145,18 @@ end
 --------------------
 local function show_osd(no_osd, label)
 	if no_osd then return end
-	mp.osd_message(('%s Shaders Set %d: %s'):format(enabled and '☑︎' or '☐', current_set, label or 'n/a'), 6)
+	mp.osd_message(('%s Shaders Set %d: %s'):format(enabled and '☑︎' or '☐', current_index, label or 'n/a'), 6)
+end
+
+local function mpv_set_options(options)
+	msg.debug('Setting Options:', utils.to_string(options))
+	for name, value in pairs(options) do
+		mp.commandv('set', name, value)
+	end
+end
+
+local function mpv_clear_options()
+	mpv_set_options(default_options())
 end
 
 local function mpv_clear_shaders()
@@ -141,13 +177,14 @@ local function clear_shaders(no_osd)
 		msg.debug('Setting Shaders: skipped - properties not available.')
 		return
 	end
-	local shaders = sets[current_set]()
+	local shaders = sets[current_index]()
 	show_osd(no_osd, shaders.label)
 	if last_shaders == nil then
 		msg.debug('Clearing Shaders: skipped - no shader found.')
 		return
 	end
 	last_shaders = nil
+	mpv_clear_options()
 	mpv_clear_shaders()
 end
 
@@ -156,7 +193,7 @@ local function set_shaders(no_osd)
 		msg.debug('Setting Shaders: skipped - properties not available.')
 		return
 	end
-	local shaders = sets[current_set]()
+	local shaders = sets[current_index]()
 	show_osd(no_osd, shaders.label)
 	if not enabled then
 		msg.debug('Setting Shaders: skipped - disabled.')
@@ -168,6 +205,7 @@ local function set_shaders(no_osd)
 		return
 	end
 	last_shaders = s
+	mpv_set_options(shaders.options)
 	mpv_set_shaders(shaders.shaders)
 end
 
@@ -177,12 +215,12 @@ end
 --------------------------
 mp.register_event('file-loaded', function()
 	reset()
-	if not o.auto_switch then return end
+	if not opts.auto_switch then return end
 	local path = mp.get_property_native('path', ''):lower()
-	current_set = o.default_index
-	if o.preset_1_enabled and path:find(o.preset_1_path) ~= nil then current_set = o.preset_1_index end
-	if o.preset_2_enabled and path:find(o.preset_2_path) ~= nil then current_set = o.preset_2_index end
-	if o.preset_3_enabled and path:find(o.preset_3_path) ~= nil then current_set = o.preset_3_index end
+	current_index = opts.default_index
+	if opts.preset_1_enabled and path:find(opts.preset_1_path) ~= nil then current_index = opts.preset_1_index end
+	if opts.preset_2_enabled and path:find(opts.preset_2_path) ~= nil then current_index = opts.preset_2_index end
+	if opts.preset_3_enabled and path:find(opts.preset_3_path) ~= nil then current_index = opts.preset_3_index end
 end)
 
 local timer = mp.add_timeout(1, function() set_shaders(true) end)
@@ -207,35 +245,35 @@ end
 --- Bindings ---
 ----------------
 local function cycle_set_up(no_osd)
-	msg.debug('Shader - Up:', current_set)
+	msg.debug('Shader - Up:', current_index)
 	if not is_initialized() then return end
-	current_set = (current_set % #sets) + 1
+	current_index = (current_index % #sets) + 1
 	set_shaders(no_osd)
 end
 
 local function cycle_set_dn(no_osd)
-	msg.debug('Shader - Down:', current_set)
+	msg.debug('Shader - Down:', current_index)
 	if not is_initialized() then return end
-	current_set = ((current_set - 2) % #sets) + 1
+	current_index = ((current_index - 2) % #sets) + 1
 	set_shaders(no_osd)
 end
 
 local function toggle_set(no_osd)
-	msg.debug('Shader - Toggling:', current_set)
+	msg.debug('Shader - Toggling:', current_index)
 	if not is_initialized() then return end
 	enabled = not enabled
 	if enabled then set_shaders(no_osd) else clear_shaders(no_osd) end
 end
 
 local function enable_set(no_osd)
-	msg.debug('Shader - Enabling:', current_set)
+	msg.debug('Shader - Enabling:', current_index)
 	if not is_initialized() then return end
 	enabled = true
 	set_shaders(no_osd)
 end
 
 local function disable_set(no_osd)
-	msg.debug('Shader - Disabling:', current_set)
+	msg.debug('Shader - Disabling:', current_index)
 	if not is_initialized() then return end
 	enabled = false
 	clear_shaders(no_osd)
