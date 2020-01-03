80449823f3d59d4069dbaede798d582bcb4ebb2f
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-01-03 09:21:57 +0700

    Subtitle_Styles - Better init; sets defauls for no sub

--- scripts/Subtitle_Styles.lua
+++ scripts/Subtitle_Styles.lua
@@ -1,4 +1,4 @@
--- deus0ww - 2019-11-06
+-- deus0ww - 2020-01-03
 
 local mp      = require 'mp'
 local msg     = require 'mp.msg'
@@ -14,7 +14,7 @@ local language_codes = {
 local language_profiles = {
 	eng = 'ww-subtitle-english',
 	tha = 'ww-subtitle-thai',
-	und = 'ww-subtitle-english',
+	und = 'ww-subtitle-thai',
 }
 
 local codec_type = {
@@ -50,22 +50,51 @@ local function get_sub_type(track_list, sid)
 		   return codec_type[track.codec:lower()]
 		end
 	end
+	return 'text'
 end
 
-mp.observe_property('sid', 'native', function(_, sid)
-	if not sid or sid == 'no' then return end
-	local track_list = mp.get_property_native('track-list', {})
-	if #track_list <= 0 then return end
-	
-	-- Set Language Style
-	local track_lang = get_sub_language(track_list, sid)
-	local profile_lang = language_profiles[track_lang]
-	mp.commandv('apply-profile', profile_lang)
-	
-	-- Set Codec Style
-	local track_type = get_sub_type(track_list, sid)
-	local profile_type = codec_profile[track_type]
-	mp.commandv('apply-profile', profile_type)
-	
-	msg.debug(('SID: %s, Language: %s, Type: %s, Profiles: %s, %s'):format(sid, track_lang, track_type, profile_lang, profile_type))
-end)
+local last = { profile_lang = 'eng', profile_type = 'text', }
+local function set_profile(track_lang, track_type)
+	local profile_lang, profile_type = language_profiles[track_lang], codec_profile[track_type]
+	if last.profile_lang ~= profile_lang then
+		last.profile_lang = profile_lang
+		msg.debug('Applying Profile:', profile_lang)
+		mp.commandv('apply-profile', profile_lang)
+	else
+		msg.debug('Skipping Profile:', profile_lang)
+	end
+	if last.profile_type ~= profile_type then
+		last.profile_type = profile_type
+		msg.debug('Applying Profile:', profile_type)
+		mp.commandv('apply-profile', profile_type)
+	else
+		msg.debug('Skipping Profile:', profile_type)
+	end
+end
+
+local p = {
+	['sid'] = 0,
+	['track-list'] = {},
+	['sub-visibility'] = false,
+}
+
+local function on_change(k, v)
+	if     k == 'sid'            then p['sid']            = v ~= nil and v or 0
+	elseif k == 'track-list'     then p['track-list']     = v or {}
+	elseif k == 'sub-visibility' then p['sub-visibility'] = v ~= nil and v or false
+	end
+	local sid, vis, track_list = p['sid'], p['sub-visibility'], p['track-list']
+	local track_lang, track_type
+	if not sid or sid == 'no' or not track_list or #track_list <= 0 or not vis then
+		track_lang, track_type = 'eng', 'text'
+	else
+		track_lang, track_type = get_sub_language(track_list, sid), get_sub_type(track_list, sid)
+	end
+	msg.debug(('Visibility: %s, SID: %s, Language: %s, Type: %s'):format(vis and 'yes' or 'no', tostring(sid), track_lang, track_type))
+	set_profile(track_lang, track_type)
+end
+
+mp.observe_property('sid', 'native', on_change)
+mp.observe_property('track-list', 'native', on_change)
+mp.observe_property('sub-visibility', 'native', on_change)
+
