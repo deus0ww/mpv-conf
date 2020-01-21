0870189e9670602139c4d21643ed6c65920470f7
deus0ww <6702585+deus0ww@users.noreply.github.com>
2020-01-21 11:38:46 +0700

    Update Thumbnailer_OSC.lua

--- scripts/Thumbnailer_OSC.lua
+++ scripts/Thumbnailer_OSC.lua
@@ -1387,7 +1387,7 @@ function render_message(ass)
         local outline = tonumber(mp.get_property("options/osd-border-size"))
         local maxlines = math.ceil(osc_param.unscaled_y*0.75 / fontsize)
         local counterscale = osc_param.playresy / osc_param.unscaled_y
-		local hidpi_scale = mp.get_property_native("display-hidpi-scale", 1.0)
+        local hidpi_scale = mp.get_property_native("display-hidpi-scale", 1.0)
 
         fontsize = fontsize * hidpi_scale * counterscale / math.max(0.65 + math.min(lines/maxlines, 1), 1)
         outline = outline * hidpi_scale * counterscale / math.max(0.75 + math.min(lines/maxlines, 1)/2, 1)
