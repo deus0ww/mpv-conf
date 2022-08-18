e35955305b7228cec6ecdecbea0c366f2943d07b
deus0ww <6702585+deus0ww@users.noreply.github.com>
2022-08-19 01:08:28 +0700

    Fixed scaletempo/rubberband osd

--- scripts/Filter_Audio_ScaleTempo.lua
+++ scripts/Filter_Audio_ScaleTempo.lua
@@ -1,16 +1,30 @@
--- deus0ww - 2020-01-21
+-- deus0ww - 2022-08-18
 
 local mp      = require 'mp'
 local msg     = require 'mp.msg'
+local utils   = require 'mp.utils'
 
 local filter_name = 'ScaleTempo'
 
 local function show_status(filter_enabled)
-	mp.osd_message(string.format('Speed: %.2f\n%s %s', mp.get_property('speed'), (filter_enabled and '■' or '□'), filter_name))
+	local filter_params = 'none'
+	for _, f in ipairs(mp.get_property_native('af')) do
+		if f.label == filter_name then
+			filter_params = f.name .. ' ' .. utils.to_string(f.params):gsub('{', '['):gsub('}', ']'):gsub(' = ', '='):gsub('"', ''):gsub(',', '')
+			break
+		end
+	end
+	mp.osd_message(string.format('Speed: %.2f\n%s %s:  %s', mp.get_property('speed'), (filter_enabled and '■' or '□'), filter_name, filter_params))
 end
 
-mp.register_script_message(filter_name .. '-enabled',  function() show_status(true)  end)
-mp.register_script_message(filter_name .. '-disabled', function() show_status(false) end)
+mp.register_script_message(filter_name .. '-enabled',  function()
+	mp.commandv('set', 'audio-pitch-correction', 'yes')
+	show_status(true)
+end)
+mp.register_script_message(filter_name .. '-disabled', function()
+	mp.commandv('set', 'audio-pitch-correction', 'no')
+	show_status(false)
+end)
 
 local previous_speed = mp.get_property_native('speed')
 
