9135f675b4eb271ded02626e59c9154b3610dd29
Deus0ww <6702585+deus0ww@users.noreply.github.com>
2023-11-02 11:14:24 +0700

    Replacing Krig with CfL

--- scripts/Shaders.lua
+++ scripts/Shaders.lua
@@ -247,7 +247,8 @@ sets[#sets+1] = function()
 	local s, o = {}, default_options()
 	s[#s+1] = ({                                      [3]=fsrcnnx2.r8,   [4]=fsrcnnx2.r16                     })[minmax_scale(3, 4)]
 	s[#s+1] = ({                                      [3]=ravu.zoom.r3s, [4]=ravu.lite.r4s, [5]=ravu.zoom.r3s })[minmax_scale(3, 5)]
-	s[#s+1] = ({[1]=bilateral.r5,  [2]=bilateral.r3,  [3]=bilateral.r4,  [4]=bilateral.r5                     })[minmax_scale(1, 4)]
+	s[#s+1] = ({[1]=bilateral.r4,  [2]=bilateral.r3,  [3]=bilateral.r4,                                       })[minmax_scale(1, 3)]
+	
 	return { shaders = s, options = o, label = 'Live' }
 end
 
@@ -255,7 +256,7 @@ sets[#sets+1] = function()
 	local s, o = {}, default_options()
 	s[#s+1] = ({                                      [3]=fsrcnnx2.r8l,  [4]=fsrcnnx2.r16e                    })[minmax_scale(3, 4)]
 	s[#s+1] = ({                                      [3]=ravu.zoom.r3s, [4]=ravu.lite.r4s, [5]=ravu.zoom.r3s })[minmax_scale(3, 5)]
-	s[#s+1] = ({[1]=bilateral.r5,  [2]=bilateral.r3,  [3]=bilateral.r4,  [4]=bilateral.r5                     })[minmax_scale(1, 4)]
+	s[#s+1] = ({[1]=bilateral.r4,  [2]=bilateral.r3,  [3]=bilateral.r4,                                       })[minmax_scale(1, 3)]
 	return { shaders = s, options = o, label = 'Rendered' }
 end
 
@@ -263,14 +264,14 @@ sets[#sets+1] = function()
 	local s, o = {}, default_options()
 	s[#s+1] = ({                                      [3]=fsrcnnx2.r8l,  [4]=fsrcnnx2.r16l                    })[minmax_scale(3, 4)]
 	s[#s+1] = ({                                      [3]=ravu.zoom.r3s, [4]=ravu.lite.r4s, [5]=ravu.zoom.r3s })[minmax_scale(3, 5)]
-	s[#s+1] = ({[1]=bilateral.r5,  [2]=bilateral.r3,  [3]=bilateral.r4,  [4]=bilateral.r5                     })[minmax_scale(1, 4)]
+	s[#s+1] = ({[1]=bilateral.r4,  [2]=bilateral.r3,  [3]=bilateral.r4,                                       })[minmax_scale(1, 3)]
 	return { shaders = s, options = o, label = 'Smooth' }
 end
 
 sets[#sets+1] = function()
 	local s, o = {}, default_options()
 	s[#s+1] = ({                                                         [4]=fsrcnnx2.r8                      })[minmax_scale(1, 4)]
-	s[#s+1] = ({[1]=ravu.zoom.r3s, [2]=ravu.lite.r3s, [3]=ravu.zoom.r2s, [4]=ravu.lite.r4s, [5]=ravu.zoom.r3s })[minmax_scale(1, 5)]
+	s[#s+1] = ({[1]=ravu.zoom.r3s, [2]=ravu.lite.r4s, [3]=ravu.zoom.r3s, [4]=ravu.lite.r4s, [5]=ravu.zoom.r3s })[minmax_scale(1, 5)]
 	s[#s+1] = bilateral.r3
 	return { shaders = s, options = o, label = 'High FPS' }
 end
@@ -279,7 +280,7 @@ sets[#sets+1] = function()
 	local s, o = {}, default_options()
 	s[#s+1] = fsrcnnx2.r16
 	s[#s+1] = ravu.zoom.r3s
-	s[#s+1] = bilateral.r5
+	s[#s+1] = bilateral.r4
 	s[#s+1] = ravu.zoom.rgb_r3s
 	s[#s+1] = igv.ssds
 	o['linear-downscaling'] = 'no'  -- for ssds
